ba97e178b683ca9d2a24c42b7b06ea87fa2638f5
Ketaki Thatte <contactketaki.thatte@gmail.com>
2015-04-19 14:21:24 +0530

    First commit to running_life

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'ffa7c2d044537099f04bc8ee91d15cee15597c19e06162ed47e192086b8111dacb16f98f24d594a61b9274506f6b7fdf48e0ac6db1ea87800b4b7900214fc464'
